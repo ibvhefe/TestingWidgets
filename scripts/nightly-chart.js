587bb46611c33edae644ecd9c79eebad36ccb371
Sebastian Mechelke <sebastian.mechelke1@sartorius.com>
2023-12-11 21:38:37 +0100

    Read tests from builds

--- scripts/nightly-chart.js
+++ scripts/nightly-chart.js
@@ -7,12 +7,48 @@ VSS.require([
     "TFS/Dashboards/WidgetHelpers", 
     "Charts/Services"
     ],
-    function (WidgetHelpers, Services) {
+    function (WidgetHelpers, Services, buildClient) {
     WidgetHelpers.IncludeWidgetStyles();
     VSS.register("nightly-chart", function () { 
          return {
          load:function() {
             return Services.ChartsService.getService().then(function(chartService){
+
+                VSS.getAccessToken().then(function(tokenObject) {
+                    var token = tokenObject.token;
+                    var projectName = VSS.getWebContext().project.name;
+                    var definitionId = 17;
+                    var branchFilter = "refs/heads/main";
+                    var organization = VSS.getWebContext().account.name;
+                    var url = `https://dev.azure.com/${organization}/${projectName}/_apis/build/builds?definitions=${definitionId}&branchName=${branchFilter}&api-version=7.1`;
+
+                    fetch(url, {
+                        method: 'GET',
+                        headers: {
+                            'Authorization': 'Bearer ' + token,
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+                        data.value.forEach(build => {
+                            var testUrl = `https://dev.azure.com/${organization}/${projectName}/_apis/test/runs?buildIds=${build.id}&api-version=7.1`;
+                            fetch(testUrl, {
+                                method: 'GET',
+                                headers: {
+                                    'Authorization': 'Bearer ' + token,
+                                    'Content-Type': 'application/json'
+                                }
+                            })
+                            .then(response => response.json())
+                            .then(testData => console.log(testData))
+                            .catch((error) => console.error('Error:', error));
+                        });
+                    })
+                    .catch((error) => console.error('Error:', error));
+                });
+
                 var $container = $('#Chart-Container');
                 var chartOptions ={ 
                     "hostOptions": { 
