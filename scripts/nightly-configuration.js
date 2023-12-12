36535867c66c01f20553d6aab3f44c4680dedafc
Sebastian Mechelke <sebastian.mechelke1@sartorius.com>
2023-12-12 23:00:51 +0100

    Update widget configuration to include dropdown options for repository, branch, and pipeline

--- scripts/nightly-configuration.js
+++ scripts/nightly-configuration.js
@@ -4,29 +4,51 @@ VSS.init({
 });
 
 VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
+    WidgetHelpers.IncludeWidgetConfigurationStyles();
     VSS.register("nightly-chart.Configuration", function () {   
-        VSS.getAccessToken()
-            .then(function(tokenObject) {
-                var token = tokenObject.token;
-                var projectName = VSS.getWebContext().project.name;
-                var organization = VSS.getWebContext().account.name;
-                var url = `https://dev.azure.com/${organization}/${projectName}/_apis/pipelines?api-version=7.1`;
-                fetch(url, 
-                {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': 'Bearer ' + token,
-                        'Content-Type': 'application/json'
-                    }
-                })
-                .then(response => response.json())
-                .then(data => console.log(data))
-                .catch((error) => console.error('Error:', error));
-            });
-
-        var $branchDropdown = $("#branch-dropdown"); 
         return {
             load: function (widgetSettings, widgetConfigurationContext) {
+        
+                var $repositoryDropdown = $("#repository-dropdown");
+                var $branchDropdown = $("#branch-dropdown");
+                var $pipelineDropdown = $("#pipeline-dropdown");
+                
+                getRepositoryData()
+                .then(repos => {
+                    repos.forEach(repo => {
+                        var option = document.createElement('option');
+                        option.text = repo.name;
+                        option.value = repo.id;
+                        $repositoryDropdown.append(option);
+                    });
+                });
+
+                $repositoryDropdown.change(function() {
+                    var selectedRepositoryId = this.value;
+                    $branchDropdown.empty(); // clear the branch dropdown
+                
+                    getBranchData(selectedRepositoryId)
+                        .then(branches => {
+                            branches.forEach(branch => {
+                                var option = document.createElement('option');
+                                option.text = branch.name;
+                                option.value = branch.id;
+                                $branchDropdown.append(option);
+                            });
+                        });
+                });
+
+                getPipelineData()
+                .then(pipelines => {
+                    pipelines.forEach(pipeline => {
+                        var option = document.createElement('option');
+                        option.text = pipeline.name;
+                        option.value = pipeline.id;
+                        console.log(option);
+                        $pipelineDropdown.append(option);
+                    });
+                });              
+
                 var settings = JSON.parse(widgetSettings.customSettings.data);
                 if (settings && settings.branch) {
                     $branchDropdown.val(settings.branch);
@@ -51,3 +73,66 @@ VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
     });
     VSS.notifyLoadSucceeded();
 });
+
+function getPipelineData() {
+    return VSS.getAccessToken()
+        .then(function(tokenObject) {
+            var token = tokenObject.token;
+            var projectName = VSS.getWebContext().project.name;
+            var organization = VSS.getWebContext().account.name;
+            var url = `https://dev.azure.com/${organization}/${projectName}/_apis/pipelines?api-version=7.1`;
+            return fetch(url, 
+            {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(data => data.value.map(pipeline => ({name: pipeline.name, id: pipeline.id})))
+            .then(pipelines => pipelines.sort((a, b) => a.name.localeCompare(b.name)));
+        });
+}
+
+function getBranchData(repositoryId) {
+    return VSS.getAccessToken()
+        .then(function(tokenObject) {
+            var token = tokenObject.token;
+            var projectName = VSS.getWebContext().project.name;
+            var organization = VSS.getWebContext().account.name;
+            var url = `https://dev.azure.com/${organization}/${projectName}/_apis/git/repositories/${repositoryId}/refs?filter=heads&api-version=6.0`;
+            return fetch(url, 
+            {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(data => data.value.map(branch => ({name: branch.name.replace('refs/heads/', ''), id: branch.objectId})))
+            .then(branches => branches.sort((a, b) => a.name.localeCompare(b.name)));
+        });
+}
+
+function getRepositoryData() {
+    return VSS.getAccessToken()
+        .then(function(tokenObject) {
+            var token = tokenObject.token;
+            var projectName = VSS.getWebContext().project.name;
+            var organization = VSS.getWebContext().account.name;
+            var url = `https://dev.azure.com/${organization}/${projectName}/_apis/git/repositories?api-version=6.0`;
+            return fetch(url, 
+            {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(data => data.value.map(repo => ({name: repo.name, id: repo.id})))
+            .then(repos => repos.sort((a, b) => a.name.localeCompare(b.name)));
+        });
+}
